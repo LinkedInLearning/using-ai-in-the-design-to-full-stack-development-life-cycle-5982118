73088de6010ff1c6f7f91e7fd93de510821d9755
Dakota Fabro <dakotafabrodev@gmail.com>
2025-02-06 19:24:21 +0000

    added faulty jest test case for demo and learning purposes

---
+++ next-app/__tests__/Home.test.js
@@ -0,0 +1,10 @@
+// import { render, screen } from '@testing-library/react';
+// import Home from '@/app/page';
+
+// describe('Home Page', () => {
+//   it('renders a heading', () => {
+//     render(<Home />);
+//     const heading = screen.getByTestId('home');
+//     expect(heading).toBeInTheDocument();
+//   // });
+// });
